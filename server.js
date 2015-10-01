2c104e7d18af2a5c06859f8f3aead5f5ea4101f8
shapingart <ashley@shapingart.com>
2015-09-30 20:33:28 -0700

    i added a bunch of verbs

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+
+
+var Adjective = function() {
+	this.Happy = true;
+	this.Lazy = true;
+	this.Crazy = true;
+	this.PMSing = true;
+	this.Cranky = true;
+}
+
+var adjective = new Adjective();
+
+function getRandomWord (object) {
+	var propArray = Object.keys(object);
+	var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
+	return {word:randomProp};
+}
+
+app.get("/", function (req,res) {
+	res.send("be happy!");
+});
+
+app.get("/adjective", function (req,res) {
+	res.json(getRandomWord(adjective));
+});
+
+
+
+app.listen(port,function()  {
+	console.log('server started on port' + port);
+});
